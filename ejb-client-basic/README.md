20910656f78b6dba44cc97b6bd55c926965fd27b
robert.lee <robert.lee@iisigroup.com>
2022-07-09 19:57:07 +0800

    add description

--- ejb-client-basic/README.md
+++ ejb-client-basic/README.md
@@ -1,4 +1,4 @@
-Remote Client EJB example for WildFly 14
+Remote Client EJB example for WildFly 26
 =====================================
 Example taken from [Practical Java EE 7 Development using WildFly application server](http://www.itbuzzpress.com/ebooks/java-ee-7-development-on-wildfly.html)
 
@@ -8,3 +8,9 @@ This example has been adapter to work on WildFly 14 and demonstrates the usage o
 ```shell
 mvn clean install exec:exec
 ```
+
+# Official Document
+   * [Redhat Official Document](https://access.redhat.com/documentation/en-us/red_hat_jboss_enterprise_application_platform/7.4/html/developing_jakarta_enterprise_beans_applications/invoking_session_beans#initial_context_lookup)
+     * [EAP 7.4 example code](https://github.com/jboss-developer/jboss-eap-quickstarts/blob/7.4.x/ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java)
+   * [Wildfly Official Document](https://docs.wildfly.org/26/Developer_Guide.html#Remote_Jakarta_Enterprise_Beans_invocations_via_JNDI_-_Jakarta_Enterprise_Beans_client_API_or_wildfly-naming-client_project)
+     * [26.1.1 example code](https://github.com/wildfly/quickstart/blob/26.1.1.Final/ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java)
--- ejb-client-basic/src/main/java/com/itbuzzpress/chapter4/client/RemoteEJBClient.java
+++ ejb-client-basic/src/main/java/com/itbuzzpress/chapter4/client/RemoteEJBClient.java
@@ -1,4 +1,4 @@
- 
+
 package com.itbuzzpress.chapter4.client;
 
 import java.util.Hashtable;
@@ -14,54 +14,59 @@ import com.itbuzzpress.chapter4.exception.InsufficientFundsException;
 
 import javax.naming.spi.NamingManager;
 import javax.rmi.PortableRemoteObject;
+
 /**
  * Reference <br/>
  * https://stackoverflow.com/questions/25051329/how-to-access-ejb-2-0-using-a-standalone-program
- * */
+ */
 public class RemoteEJBClient {
 
 	public static void main(String[] args) throws Exception {
-		Context context =   createInitialContextV2();
-  
+		Context context = createInitialContextV2();
+
 		try {
-			java.lang.Object obj = context.lookup("ejb:/ejb2-server-basic/AdviceBean!com.itbuzzpress.chapter4.ejb.Advice");
-		    
-		    // Old EJB2 style migrate to EJB3 style
+			java.lang.Object obj = context
+					.lookup("ejb:/ejb2-server-basic/AdviceBean!com.itbuzzpress.chapter4.ejb.Advice");
+
+			// Old EJB2 style migrate to EJB3 style
 //		    AdviceHome home = (AdviceHome) javax.rmi.PortableRemoteObject.narrow(obj, AdviceHome.class); //This is EJB2 style 
 //		    Advice advisor = home.create();
-		    
-		    Advice advisor = (Advice)obj ; //EJB 3 style
-		    System.out.println(advisor.getAdvice());
-		}finally {
-			//https://docs.jboss.org/author/display/WFLY/Scoped%20EJB%20client%20contexts.html
+
+			Advice advisor = (Advice) obj; // EJB 3 style
+			System.out.println(advisor.getAdvice());
+		} finally {
+			// https://docs.jboss.org/author/display/WFLY/Scoped%20EJB%20client%20contexts.html
 			if (context != null) {
 				context.close();
-			} 
+			}
 		}
-		
-
 
 	}
 
-	private static  Context createInitialContextV2() throws NamingException {
-	final Hashtable<String, String> jndiProperties = new Hashtable<>();
-    jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-    //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-    jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
-//  jndiProperties.put(SECURITY_PRINCIPAL, "admin");
-//	jndiProperties.put(SECURITY_CREDENTIALS, "secret123!");     
-    
-    return new InitialContext(jndiProperties);
-}
-private  static Context createInitialContextV1() throws NamingException {
-    Properties jndiProperties = new Properties();
-    jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-    jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-    jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
-    jndiProperties.put("jboss.naming.client.ejb.context", true);
+	/**
+	 * https://access.redhat.com/documentation/en-us/red_hat_jboss_enterprise_application_platform/7.4/html/developing_jakarta_enterprise_beans_applications/invoking_session_beans#initial_context_lookup
+	 * **/
+	private static Context createInitialContextV2() throws NamingException {
+		final Hashtable<String, String> jndiProperties = new Hashtable<>();
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+		// use HTTP upgrade, an initial upgrade requests is sent to upgrade to the
+		// remoting protocol
+		jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+		// jndiProperties.put(SECURITY_PRINCIPAL, "admin");
+		// jndiProperties.put(SECURITY_CREDENTIALS, "secret123!");
+
+		return new InitialContext(jndiProperties);
+	}
+
+	private static Context createInitialContextV1() throws NamingException {
+		Properties jndiProperties = new Properties();
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		jndiProperties.put("jboss.naming.client.ejb.context", true);
 //  jndiProperties.put(SECURITY_PRINCIPAL, "admin");
 //	jndiProperties.put(SECURITY_CREDENTIALS, "secret123!");
-  
-    return new InitialContext(jndiProperties);
-}
+
+		return new InitialContext(jndiProperties);
+	}
 }
